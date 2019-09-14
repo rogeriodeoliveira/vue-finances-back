b2daa79c7c9342ab84212d3808b757282d7121fd
rogeriodeoliveira <blogtecnologiacominformacao@gmail.com>
2019-09-14 10:43:24 -0300

    ajustes

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken')
 
-function getUserId(context){
+function getUserId (context) {
   const Authorization = context.request.get('Authorization')
-  if(Authorization){
+  if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
     const { userId } = jwt.verify(token, process.env.JWT_SECRET)
     return userId
   }
-  throw new Error('Not Authenticated')
+
+  throw new Error('Not authenticated!')
 }
 
 module.exports = {
